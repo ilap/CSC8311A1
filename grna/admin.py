bc1302a7045f82fd4a5e6aff4148db76c9b550d5
ilap <pal.dorogi@gmail.com>
2016-03-04 08:48:45 +0000

    Playing with view.

--- grna/admin.py
+++ grna/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import GuideRNA
+from .models import *
 
 admin.site.register(GuideRNA)
+admin.site.register(Question)
--- grna/views.py
+++ grna/views.py
@@ -4,23 +4,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-
-from .models import GuideRNA
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import *
 
 def iindex(request):
-    latest_question_list = GuideRNA.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('grna/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
 
-def indea2x(request):
-    latest_question_list = GuideRNA.objects.order_by('-pub_date')[:5]
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
@@ -33,3 +29,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
