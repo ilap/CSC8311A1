800a01d081a8a1df90c7a8a843b0cde881b4e2d5
ilap <pal.dorogi@gmail.com>
2016-03-05 21:31:14 +0000

    Main page is added.

--- grna/urls.py
+++ grna/urls.py
@@ -4,13 +4,8 @@ from . import views
 
 app_name = 'grna'
 urlpatterns = [
-    # ex: /grna/
-    url(r'^$', views.grna_query, name='grna_query'),
-    #url(r'^$', views.index, name='index'),
-    # ex: /grna/5/
-    #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /grna/5/results/
-    #url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /grna/5/vote/
-    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # The initial view for designing new gRNA
+    url(r'^$', views.grna_new, name='grna_new'),
+    # The results of the query
+    url(r'^results/$', views.grna_results, name='grna_results'),
 ]
