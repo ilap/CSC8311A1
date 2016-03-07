b20797f642bd646f2294de8bb78428b13a95ef9a
ilap <pal.dorogi@gmail.com>
2016-03-07 11:08:36 +0000

    View function grna_new is converted to a class-based views called GuideRNAView

--- grna/urls.py
+++ grna/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 app_name = 'grna'
 urlpatterns = [
     # The initial view for designing new gRNA
-    url(r'^$', views.grna_new, name='grna_new'),
+    url(r'^$', views.GuideRNAView.as_view()),
     # The results of the query
     url(r'^results/$', views.grna_results, name='grna_results'),
 ]
--- grna/views.py
+++ grna/views.py
@@ -1,32 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views.generic import View
 
 from .forms import *
 from .models import *
 
+class GuideRNAView(View):
+    form_class = SpeciesForm
+    template_new = 'grna/grna_new.html'
+    template_results = 'grna/grna_results.html'
 
-def grna_new(request):
-
-    if request.POST.get('upload'):
-        print "Upload"
-        form = SpeciesForm(request.POST, request.FILES)
-        if form.is_valid():
-            # file is saved
-            species = Species(fasta_file=request.FILES['fasta_file'])
-            species.save()
-
-            # Redirect to the document list after POST
-            return HttpResponseRedirect(reverse('myapp.views.list'))
-        return render(request, 'grna/grna_new.html', {})
-
-    elif request.POST.get('search'):
-        print "DO stuffs"
-        return render(request, 'grna/grna_results.html', {})
-
-    else:
-        form = SpeciesForm()
-
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
         species = Species.objects.all()
         nucleases = Nuclease.objects.all()
         pams = PAM.objects.all()
@@ -34,11 +20,26 @@ def grna_new(request):
         print species
         print nucleases
         print pams
+        return render(request, self.template_new, {'form': form,
+                                              'species': species,
+                                              'nucleases': nucleases,
+                                              'pams': pams, })
 
-        return render(request, 'grna/grna_new.html', {'form': form,
-                                                      'species': species,
-                                                      'nucleases': nucleases,
-                                                      'pams': pams, })
+    def post(self, request, *args, **kwargs):
+        if request.POST.get('upload'):
+            print "Upload"
+            form = self.form_class(request.POST, request.FILES)
+            if form.is_valid():
+                # file is saved
+                species = Species(fasta_file=request.FILES['fasta_file'])
+                species.save()
+
+                # Redirect to the document list after POST
+                return HttpResponseRedirect(reverse('myapp.views.list'))
+            return render(request, self.template_new, {})
+        elif request.POST.get('search'):
+            print "DO stuffs"
+            return render(request, self.template_results, {})
 
 
 def grna_results(request):
